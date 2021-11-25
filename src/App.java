3bb5761b90c8ae1f76c26a3a5f219f09da484c36
Huma Prathama <huma.elektro@gmail.com>
2021-11-25 08:30:16 +0800

    edit test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hallo angkatan 2020 TI");
+        System.out.println("Hallo angkatan 2020 TI clone");
     }
 }
